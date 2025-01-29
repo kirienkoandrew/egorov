d381ce413717e57531a1850444d59cf397c54b2c
Andrew Kirienko <kir.andrew@gmail.com>
2025-01-29 13:57:53 +0700

    done

--- step4/less10.py
+++ step4/less10.py
@@ -1,7 +1,23 @@
-class Point:
-    pass
+class Person:
+    def __init__(self, name, surname, gender='male'):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
 
+    @property
+    def gender(self):
+        return self.__gender
 
-p1 = Point()
+    @gender.setter
+    def gender(self, value):
+        if value in ('male', 'female'):
+            self.__gender = value
+        else:
+            print('Не знаю, что вы имели в виду? Пусть это будет мальчик!')
+            self.__gender = 'male'
 
-print(p1.__str__())
+    def __str__(self):
+        if self.gender == 'male':
+            return f'Гражданин {self.surname} {self.name}'
+        else:
+            return f'Гражданка {self.surname} {self.name}'
