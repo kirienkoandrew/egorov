b66c21ec5028fc9b044f84cec843d867ecddf581
Andrew Kirienko <kir.andrew@gmail.com>
2025-01-29 17:18:37 +0700

    __add__ done

---
+++ step4/less22.py
@@ -0,0 +1,29 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def __add__(self, other):
+        if isinstance(other, Rectangle):
+            return Rectangle(self.width + other.width, self.height + other.height)
+        else:
+            raise TypeError
+
+    def __str__(self):
+        return f'Rectangle({self.width}x{self.height})'
+
+r1 = Rectangle(5, 10)
+assert r1.width == 5
+assert r1.height == 10
+print(r1)
+
+r2 = Rectangle(20, 5)
+assert r2.width == 20
+assert r2.height == 5
+print(r2)
+
+r3 = r2 + r1
+assert isinstance(r3, Rectangle)
+assert r3.width == 25
+assert r3.height == 15
+print(r3)
